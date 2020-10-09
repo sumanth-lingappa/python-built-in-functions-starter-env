f09617778e3b9f737ab86b12fd07bce3c0a5f9c0
Sumanth Lingappa <sumanth.lingappa@citrix.com>
2020-10-09 16:40:06 +0530

    added test script

---
+++ python_builtin_functions.py
@@ -0,0 +1,88 @@
+def myabs(number):
+    """ Implementation of built-in `abs` function """
+    pass
+
+
+def myall(iterable):
+    """ Implementation of built-in `all` function """
+    pass
+
+
+def myany(iterable):
+    """ Implementation of built-in `any` function """
+    pass
+
+
+def mylen(s):
+    """ Implementation of built-in `len` function """
+    pass
+
+
+def mysum(iterable):
+    """ Implementation of built-in `sum` function """
+    pass
+
+
+def mymax(iterable):
+    """ Implementation of built-in `max` function """
+    pass
+
+
+def mymin(iterable):
+    """ Implementation of built-in `min` function """
+    pass
+
+
+def mydivmod(a, b):
+    """ Implementation of built-in `divmod` function """
+    pass
+
+
+def mybool(x):
+    """ Implementation of built-in `bool` function """
+    pass
+
+
+def mypow(base, exp):
+    """ Implementation of built-in `pow` function """
+    pass
+
+
+def myreversed(seq):
+    """ Implementation of built-in `reversed` function """
+    pass
+
+
+def myenumerate(iterable, start=0):
+    """ Implementation of built-in `enumerate` function """
+    pass
+
+
+def mybin(x):
+    """ Implementation of built-in `bin` function """
+    pass
+
+
+def myhex(x):
+    """ Implementation of built-in `hex` function """
+    pass
+
+
+def myoct(x):
+    """ Implementation of built-in `oct` function """
+    pass
+
+
+def myisinstance(object, classinfo):
+    """ Implementation of built-in `isinstance` function """
+    pass
+
+
+def myround(number, ndigits=None):
+    """ Implementation of built-in `round` function """
+    pass
+
+
+def mysorted(iterable, reverse=False):
+    """ Implementation of built-in `sorted` function """
+    pass
